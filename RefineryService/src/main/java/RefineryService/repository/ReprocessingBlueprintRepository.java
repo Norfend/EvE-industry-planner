980ff0d342c288fe08aa670c19e3f69bc594820d
Egor Uporov <86573982+Norfend@users.noreply.github.com>
2024-08-11 00:33:09 +0200

    Merging changes made (#14)
    * Added ability to accept GET requests with body in JSON format and request from “raw resources” table
    
    * The issue is on hold because I need to change the database schemas

---
+++ RefineryService/src/main/java/RefineryService/repository/ReprocessingBlueprintRepository.java
@@ -0,0 +1,7 @@
+package RefineryService.repository;
+
+import RefineryService.model.ReprocessingBlueprint;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReprocessingBlueprintRepository extends JpaRepository<ReprocessingBlueprint, Long> {
+}
