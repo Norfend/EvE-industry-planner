7f012676baeaca72122b2aa09d2f22b704e2d4b3
Egor Uporov <86573982+Norfend@users.noreply.github.com>
2024-08-13 20:32:48 +0200

    End of issues #16 and 21
    * Ores with "Batch" in the name were excluded from parsing
    * Corrected database completion: now each "Raw Resource" additionally has a column with a skill that affects the efficiency of its reprocessing.
    All necessary ore groups and their corresponding skills have been added to a separate method. I can't figure out a more elegant way to do this

--- SettingsService/src/main/java/SettingsService/model/MaterialsAfterReprocessing.java
+++ SettingsService/src/main/java/SettingsService/model/MaterialsAfterReprocessing.java
@@ -1,58 +1,30 @@
 package SettingsService.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "\"materials after reprocessing\"",
         uniqueConstraints = {
                 @UniqueConstraint(name = "\"unique materials after reprocessing iconID\"",
-                                  columnNames = {"\"materials after reprocessing icon id\""}),
+                        columnNames = {"\"materials after reprocessing icon id\""}),
                 @UniqueConstraint(name = "\"unique materials after reprocessing name\"",
-                                  columnNames = {"\"materials after reprocessing name\""})})
+                        columnNames = {"\"materials after reprocessing name\""})})
 public class MaterialsAfterReprocessing {
-    public MaterialsAfterReprocessing() {
-    }
-
-    public MaterialsAfterReprocessing(Long materialsAfterReprocessingId,
-                                      int materialsAfterReprocessingIconID,
-                                      String materialsAfterReprocessingName) {
-        MaterialsAfterReprocessingId = materialsAfterReprocessingId;
-        MaterialsAfterReprocessingIconID = materialsAfterReprocessingIconID;
-        MaterialsAfterReprocessingName = materialsAfterReprocessingName;
-    }
-
     @Id
     @Column(name = "\"materials after reprocessing id\"")
-    private Long MaterialsAfterReprocessingId;
+    private Long materialsAfterReprocessingId;
 
     @Column(name = "\"materials after reprocessing icon id\"", nullable = false, unique = true)
-    private int MaterialsAfterReprocessingIconID;
+    private int materialsAfterReprocessingIconID;
 
     @Column(name = "\"materials after reprocessing name\"", nullable = false, unique = true)
-    private String MaterialsAfterReprocessingName;
-
-    public Long getMaterialsAfterReprocessingId() {
-        return MaterialsAfterReprocessingId;
-    }
-
-    public void setMaterialsAfterReprocessingId(Long materialsAfterReprocessingId) {
-        MaterialsAfterReprocessingId = materialsAfterReprocessingId;
-    }
-
-    public int getMaterialsAfterReprocessingIconID() {
-        return MaterialsAfterReprocessingIconID;
-    }
-
-    public void setMaterialsAfterReprocessingIconID(int materialsAfterReprocessingIconID) {
-        MaterialsAfterReprocessingIconID = materialsAfterReprocessingIconID;
-    }
-
-    public String getMaterialsAfterReprocessingName() {
-        return MaterialsAfterReprocessingName;
-    }
-
-    public void setMaterialsAfterReprocessingName(String materialsAfterReprocessingName) {
-        MaterialsAfterReprocessingName = materialsAfterReprocessingName;
-    }
+    private String materialsAfterReprocessingName;
 }
--- SettingsService/src/main/java/SettingsService/model/RawResources.java
+++ SettingsService/src/main/java/SettingsService/model/RawResources.java
@@ -1,67 +1,33 @@
 package SettingsService.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "\"raw resources\"",
         uniqueConstraints = {
-        @UniqueConstraint(name = "\"unique raw resource name\"", columnNames = {"\"raw resource name\""})})
+                @UniqueConstraint(name = "\"unique raw resource name\"", columnNames = {"\"raw resource name\""})})
 public class RawResources {
-    public RawResources() {
-    }
-
-    public RawResources(int rawResourcePortionSize,
-                        String rawResourceName,
-                        int rawResourceIconID,
-                        Long rawResourceId) {
-        RawResourcePortionSize = rawResourcePortionSize;
-        RawResourceName = rawResourceName;
-        RawResourceIconID = rawResourceIconID;
-        RawResourceId = rawResourceId;
-    }
-
     @Id
     @Column(name = "\"raw resource id\"")
-    private Long RawResourceId;
+    private Long rawResourceId;
 
     @Column(name = "\"raw resource icon id\"", nullable = false)
-    private int RawResourceIconID;
+    private int rawResourceIconID;
 
     @Column(name = "\"raw resource name\"", nullable = false, unique = true)
-    private String RawResourceName;
+    private String rawResourceName;
 
     @Column(name = "\"raw resource portion size\"", nullable = false)
-    private int RawResourcePortionSize;
+    private int rawResourcePortionSize;
 
-    public int getRawResourcePortionSize() {
-        return RawResourcePortionSize;
-    }
-
-    public void setRawResourcePortionSize(int rawResourcePortionSize) {
-        RawResourcePortionSize = rawResourcePortionSize;
-    }
-
-    public String getRawResourceName() {
-        return RawResourceName;
-    }
-
-    public void setRawResourceName(String rawResourceName) {
-        RawResourceName = rawResourceName;
-    }
-
-    public int getRawResourceIconID() {
-        return RawResourceIconID;
-    }
-
-    public void setRawResourceIconID(int rawResourceIconID) {
-        RawResourceIconID = rawResourceIconID;
-    }
-
-    public Long getRawResourceId() {
-        return RawResourceId;
-    }
-
-    public void setRawResourceId(Long rawResourceId) {
-        RawResourceId = rawResourceId;
-    }
+    @Column(name = "\"raw resource refinery skill\"", nullable = false)
+    private String rawResourceRefinerySkill;
 }
